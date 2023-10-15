23287098d69ef0e63fa1da6fcc1b7ba602da8b78
Simon Norberg <simon@pthread.se>
2023-10-15 04:39:33 +0200

    Script to save the value to postgres

---
+++ db-persister.py
@@ -0,0 +1,77 @@
+import configparser
+import psycopg2
+from datetime import datetime, timedelta
+from ecoguard.ecoguard_client import EcoGuardClient
+from ecoguard.utl_type import UtlType
+from pprint import pprint
+
+
+def insert_data_to_db(cursor, node, func, unit, utl, timestamp, value):
+    insert_query = """
+    INSERT INTO node_data (node_id, node_name, func, unit, utl, timestamp, value)
+    VALUES (%s, %s, %s, %s, %s, %s, %s);
+    """
+    cursor.execute(insert_query, (node['ID'], node['Name'], func, unit, utl, timestamp, value))
+
+def main():
+    # Load Configurations
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    # EcoGuard API configurations
+    ecoguard_username = config['ECOGUARD']['USERNAME']
+    ecoguard_password = config['ECOGUARD']['PASSWORD']
+    ecoguard_domain = config['ECOGUARD']['DOMAIN']
+    
+    # PostgreSQL configurations
+    db_host = config['POSTGRES']['HOST']
+    db_name = config['POSTGRES']['DATABASE']
+    db_user = config['POSTGRES']['USER']
+    db_password = config['POSTGRES']['PASSWORD']
+
+    # Initialize EcoGuardClient
+    client = EcoGuardClient(ecoguard_username, ecoguard_password, ecoguard_domain)
+
+    # Connect to PostgreSQL
+    conn = psycopg2.connect(
+        dbname=db_name,
+        user=db_user,
+        password=db_password,
+        host=db_host
+    )
+    cursor = conn.cursor()
+    
+    try:
+        # Fetch data from EcoGuard API
+        user_info = client.get_user_info()
+        node_id = user_info.get('NodeID')
+        from_datetime = datetime.utcnow() - timedelta(days=1)
+        to_datetime = datetime.utcnow()
+
+        for utl_type in UtlType:
+            try:
+                node_data = client.get_node_data(node_id, utl_type, from_datetime, to_datetime)
+                pprint(node_data)
+                for node in node_data:
+                    for result in node['Result']:
+                        func = result['Func']
+                        unit = result['Unit']
+                        utl = result['Utl']
+                        for value_entry in result['Values']:
+                            timestamp = value_entry['Time']
+                            value = value_entry['Value']
+                            insert_data_to_db(cursor, node, func, unit, utl, timestamp, value)
+                
+                # Commit database transaction
+                conn.commit()
+                
+            except Exception as e:
+                print(f"Failed to fetch or insert data for {utl_type.name}: {str(e)}")
+                conn.rollback()  # Revert changes on error
+    finally:
+        # Close database connection
+        cursor.close()
+        conn.close()
+
+if __name__ == "__main__":
+    main()
